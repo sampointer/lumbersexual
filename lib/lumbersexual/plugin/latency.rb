48eb306dca8522c2578ef66c73896277fdbdd673
sam <Sam Pointer>
2016-04-18 11:05:37 +0100

    stub latency plugin

--- lib/lumbersexual/plugin/latency.rb
+++ lib/lumbersexual/plugin/latency.rb
@@ -3,33 +3,43 @@
 require "statsd-ruby"
 require "securerandom"
 require "uri"
+require "syslog"
+require "timeout"
 
 module Lumbersexual
   module Plugin
     class Latency
       def initialize(options, *args)
         @options = options
+        @found = false
       end
 
       def perform
-        while true do
-          uuid = SecureRandom.uuid
-          statsd = Statsd.new(@options[:statsdhost]).tap { |s| s.namespace = "lumbersexual.latency" } if @options[:statsdhost]
-          @start_time = Time.now
-          syslog = Syslog.open('lumbersexual-ping', Syslog::LOG_CONS | Syslog::LOG_NDELAY | Syslog::LOG_PID, SYSLOG::LOG_INFO)
+        uuid = SecureRandom.uuid
+        @start_time = Time.now
+        Timeout::timeout(@options[:timeout]) {
+          syslog = Syslog.open('lumbersexual-ping', Syslog::LOG_CONS | Syslog::LOG_NDELAY | Syslog::LOG_PID, Syslog::LOG_INFO)
           syslog.log(Syslog::LOG_WARNING, "PING #{uuid}")
           syslog.close
 
           # Search index for message
           #
+        }
 
-          @end_time = Time.now
-        end
+        @end_time = Time.now
+        raise Interrupt
       end
 
       def report
-        puts "Latency: #{@end_time - @start_time}"
-        statsd.gauge 'latency', @end_time - @start_time if @options[:statsdhost]
+        statsd = Statsd.new(@options[:statsdhost]).tap { |s| s.namespace = "lumbersexual.latency" } if @options[:statsdhost]
+
+        if @found
+          puts "Latency: #{@end_time - @start_time}"
+          statsd.gauge 'rtt', @end_time - @start_time if @options[:statsdhost]
+        else
+          statsd.gauge 'runs.failed', 1
+          puts "Latency: unknown, message not found"
+        end
       end
     end
   end
